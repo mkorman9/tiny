7d974032aa1d0e4607f9212ab6382fbe02ee5544
Michał Korman <michal.korman@icloud.com>
2022-11-19 17:11:00 +0100

    add unit tests to httpauth

---
+++ tinyhttp/httpauth/httpauth_test.go
@@ -0,0 +1,129 @@
+package httpauth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mkorman9/tiny"
+	"github.com/mkorman9/tiny/tinyhttp"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func init() {
+	tiny.SetupLogger()
+}
+
+func TestMissingToken(t *testing.T) {
+	// given
+	responsePayload := "payload"
+	correctToken := "token"
+
+	middleware := createBearerTokenMiddleware(correctToken)
+
+	engine := tinyhttp.NewServer().Engine
+	engine.GET(
+		"/secured",
+		middleware.AnyOfRoles("ADMIN"),
+		func(c *gin.Context) {
+			c.String(http.StatusOK, responsePayload)
+		},
+	)
+
+	// when
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/secured", nil)
+	engine.ServeHTTP(recorder, req)
+
+	// then
+	assert.Equal(t, http.StatusUnauthorized, recorder.Code, "response code should be 401")
+}
+
+func TestInvalidToken(t *testing.T) {
+	// given
+	responsePayload := "payload"
+	correctToken := "token"
+
+	middleware := createBearerTokenMiddleware(correctToken)
+
+	engine := tinyhttp.NewServer().Engine
+	engine.GET(
+		"/secured",
+		middleware.AnyOfRoles("ADMIN"),
+		func(c *gin.Context) {
+			c.String(http.StatusOK, responsePayload)
+		},
+	)
+
+	// when
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/secured", nil)
+	req.Header.Set("Authorization", "Bearer incorrectToken")
+	engine.ServeHTTP(recorder, req)
+
+	// then
+	assert.Equal(t, http.StatusUnauthorized, recorder.Code, "response code should be 401")
+}
+
+func TestValidToken(t *testing.T) {
+	// given
+	responsePayload := "payload"
+	correctToken := "token"
+
+	middleware := createBearerTokenMiddleware(correctToken)
+
+	engine := tinyhttp.NewServer().Engine
+	engine.GET(
+		"/secured",
+		middleware.AnyOfRoles("ADMIN"),
+		func(c *gin.Context) {
+			c.String(http.StatusOK, responsePayload)
+		},
+	)
+
+	// when
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/secured", nil)
+	req.Header.Set("Authorization", "Bearer "+correctToken)
+	engine.ServeHTTP(recorder, req)
+
+	// then
+	assert.Equal(t, http.StatusOK, recorder.Code, "response code should be 200")
+	assert.Equal(t, []byte(responsePayload), recorder.Body.Bytes(), "response payload should match")
+}
+
+func TestInvalidRoles(t *testing.T) {
+	// given
+	responsePayload := "payload"
+	correctToken := "token"
+
+	middleware := createBearerTokenMiddleware(correctToken)
+
+	engine := tinyhttp.NewServer().Engine
+	engine.GET(
+		"/secured",
+		middleware.AnyOfRoles("SUPERUSER"),
+		func(c *gin.Context) {
+			c.String(http.StatusOK, responsePayload)
+		},
+	)
+
+	// when
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/secured", nil)
+	req.Header.Set("Authorization", "Bearer "+correctToken)
+	engine.ServeHTTP(recorder, req)
+
+	// then
+	assert.Equal(t, http.StatusForbidden, recorder.Code, "response code should be 403")
+}
+
+func createBearerTokenMiddleware(correctToken string) *Middleware {
+	return NewBearerTokenMiddleware(func(c *gin.Context, token string) (*VerificationResult, error) {
+		if token == correctToken {
+			return &VerificationResult{Verified: true, Roles: []string{"ADMIN"}}, nil
+		} else {
+			return &VerificationResult{}, nil
+		}
+	})
+}
