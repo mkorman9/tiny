fd85a57c3d55e353f37f196a39d59a94772fb22d
Michał Korman <michal.korman@icloud.com>
2023-01-08 16:38:27 +0100

    add unit tests for framing protocols in tinytcp

---
+++ tinytcp/framing_test.go
@@ -0,0 +1,133 @@
+package tinytcp
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSeparatorFraming(t *testing.T) {
+	// given
+	protocol := SplitBySeparator([]byte{'\n'})
+	payload := generateTestPayloadWithSeparator(128)
+
+	// when
+	packet, rest, extracted := protocol.ExtractPacket(payload)
+
+	// then
+	assert.True(t, extracted, "packet should be extracted")
+	assert.True(t, validateTestPayload(128, packet), "packet should be valid")
+	assert.Len(t, rest, 0, "packet should be only data in buffer")
+}
+
+func TestVarIntPrefixFraming(t *testing.T) {
+	// given
+	protocol := LengthPrefixedFraming(PrefixVarInt)
+	payload := generateVarIntTestPayload(128)
+
+	// when
+	packet, rest, extracted := protocol.ExtractPacket(payload)
+
+	// then
+	assert.True(t, extracted, "packet should be extracted")
+	assert.True(t, validateTestPayload(128, packet), "packet should be valid")
+	assert.Len(t, rest, 0, "packet should be only data in buffer")
+}
+
+func TestInt16PrefixFraming(t *testing.T) {
+	// given
+	protocol := LengthPrefixedFraming(PrefixInt16_BE)
+	payload := generateInt16TestPayload(128)
+
+	// when
+	packet, rest, extracted := protocol.ExtractPacket(payload)
+
+	// then
+	assert.True(t, extracted, "packet should be extracted")
+	assert.True(t, validateTestPayload(128, packet), "packet should be valid")
+	assert.Len(t, rest, 0, "packet should be only data in buffer")
+}
+
+func TestInt32PrefixFraming(t *testing.T) {
+	// given
+	protocol := LengthPrefixedFraming(PrefixInt32_BE)
+	payload := generateInt32TestPayload(128)
+
+	// when
+	packet, rest, extracted := protocol.ExtractPacket(payload)
+
+	// then
+	assert.True(t, extracted, "packet should be extracted")
+	assert.True(t, validateTestPayload(128, packet), "packet should be valid")
+	assert.Len(t, rest, 0, "packet should be only data in buffer")
+}
+
+func TestInt64PrefixFraming(t *testing.T) {
+	// given
+	protocol := LengthPrefixedFraming(PrefixInt64_BE)
+	payload := generateInt64TestPayload(128)
+
+	// when
+	packet, rest, extracted := protocol.ExtractPacket(payload)
+
+	// then
+	assert.True(t, extracted, "packet should be extracted")
+	assert.True(t, validateTestPayload(128, packet), "packet should be valid")
+	assert.Len(t, rest, 0, "packet should be only data in buffer")
+}
+
+func generateTestPayloadWithSeparator(n int) []byte {
+	var buff bytes.Buffer
+	_ = WriteBytes(&buff, generateTestPayload(n))
+	_ = WriteByte(&buff, '\n')
+	return buff.Bytes()
+}
+
+func generateVarIntTestPayload(n int) []byte {
+	var buff bytes.Buffer
+	_ = WriteByteArray(&buff, generateTestPayload(n))
+	return buff.Bytes()
+}
+
+func generateInt16TestPayload(n int) []byte {
+	var buff bytes.Buffer
+	_ = WriteInt16(&buff, int16(n))
+	_ = WriteBytes(&buff, generateTestPayload(n))
+	return buff.Bytes()
+}
+
+func generateInt32TestPayload(n int) []byte {
+	var buff bytes.Buffer
+	_ = WriteInt32(&buff, int32(n))
+	_ = WriteBytes(&buff, generateTestPayload(n))
+	return buff.Bytes()
+}
+
+func generateInt64TestPayload(n int) []byte {
+	var buff bytes.Buffer
+	_ = WriteInt64(&buff, int64(n))
+	_ = WriteBytes(&buff, generateTestPayload(n))
+	return buff.Bytes()
+}
+
+func generateTestPayload(n int) []byte {
+	buff := make([]byte, n)
+	for i := range buff {
+		buff[i] = 'A'
+	}
+	return buff
+}
+
+func validateTestPayload(n int, payload []byte) bool {
+	if len(payload) != n {
+		return false
+	}
+
+	for _, v := range payload {
+		if v != 'A' {
+			return false
+		}
+	}
+
+	return true
+}
