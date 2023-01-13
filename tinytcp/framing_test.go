34f1de34ce13def8d51731137ca90522f990460f
Michał Korman <michal.korman@icloud.com>
2023-01-13 15:42:41 +0100

    add unit test for too big packet in PacketFramingHandler

--- tinytcp/framing_test.go
+++ tinytcp/framing_test.go
@@ -61,6 +61,30 @@ func TestFramingHandlerFragmentedPacket(t *testing.T) {
 	assert.Equal(t, 1, receivedPackets, "received packets count must match")
 }
 
+func TestFramingHandlerPacketTooBig(t *testing.T) {
+	// given
+	in := bytes.NewBuffer(generateTestPayloadWithSeparator(1024))
+	socket := MockConnectedSocket(in, io.Discard)
+
+	// when
+	var receivedPackets int
+
+	PacketFramingHandler(
+		SplitBySeparator([]byte{'\n'}),
+		func(ctx *PacketFramingContext) {
+			// then
+			assert.Equal(t, socket, ctx.Socket(), "context must hold the original socket")
+
+			ctx.OnPacket(func(packet []byte) {
+				receivedPackets++
+			})
+		},
+		MaxPacketSize(512),
+	)(socket)
+
+	assert.Equal(t, 0, receivedPackets, "received packets count must match")
+}
+
 func TestSeparatorFraming(t *testing.T) {
 	// given
 	protocol := SplitBySeparator([]byte{'\n'})
