a0905d5fec9c937a0b79eb9b454d6c1c69345dac
Michał Korman <michal.korman@icloud.com>
2023-01-13 15:35:42 +0100

    add unit test for fragmented packet in PacketFramingHandler

--- tinytcp/framing_test.go
+++ tinytcp/framing_test.go
@@ -36,6 +36,31 @@ func TestFramingHandlerSimple(t *testing.T) {
 	assert.Equal(t, 1, receivedPackets, "received packets count must match")
 }
 
+func TestFramingHandlerFragmentedPacket(t *testing.T) {
+	// given
+	in := bytes.NewBuffer(generateTestPayloadWithSeparator(1024))
+	socket := MockConnectedSocket(in, io.Discard)
+
+	// when
+	var receivedPackets int
+
+	PacketFramingHandler(
+		SplitBySeparator([]byte{'\n'}),
+		func(ctx *PacketFramingContext) {
+			// then
+			assert.Equal(t, socket, ctx.Socket(), "context must hold the original socket")
+
+			ctx.OnPacket(func(packet []byte) {
+				receivedPackets++
+				assert.True(t, validateTestPayload(1024, packet), "packet should be valid")
+			})
+		},
+		ReadBufferSize(512),
+	)(socket)
+
+	assert.Equal(t, 1, receivedPackets, "received packets count must match")
+}
+
 func TestSeparatorFraming(t *testing.T) {
 	// given
 	protocol := SplitBySeparator([]byte{'\n'})
